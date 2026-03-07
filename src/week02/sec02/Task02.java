39456f6db59f9d02a88d65f891a8e7ce7843d841
RootUnvalue <mls1218@naver.com>
2026-03-08 05:41:38 +0900

    update

--- src/week02/sec02/Task02.java
+++ src/week02/sec02/Task02.java
@@ -4,6 +4,5 @@ public class Task02 {
     public static void main(String[] args) {
         System.out.print("첫 Java 프로그램 입니다.");
         System.out.print("화면에 출력하고 있습니다.");
-        return;
     }
 }
