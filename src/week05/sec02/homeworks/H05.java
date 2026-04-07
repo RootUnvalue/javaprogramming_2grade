1989de73cba803406556c0cf7225b8f679b7074f
RootUnvalue <mls1218@naver.com>
2026-04-07 20:10:31 +0900

    update

--- src/week05/sec02/homeworks/H05.java
+++ src/week05/sec02/homeworks/H05.java
@@ -8,10 +8,23 @@ public class H05 {
         System.out.println("점수를 입력하세요.");
         int score = input.nextInt();
         String grade = switch (score/10) {
-            case 9 -> (score >= 95) ? "A+":"A";
-            case 8 -> (score >= 85) ? "B+":"B";
-            case 7 -> (score >= 75) ? "C+":"C";
-            case 6 -> (score >= 65) ? "D+":"D";
+            case 10 -> "A+";
+            case 9 -> switch (score%10) {
+                case 5,6,7,8,9 -> "A+";
+                default -> "A";
+            };
+            case 8 -> switch (score%10) {
+                case 5,6,7,8,9 -> "B+";
+                default -> "B";
+            };
+            case 7 -> switch (score%10) {
+                case 5,6,7,8,9 -> "C+";
+                default -> "C";
+            };
+            case 6 -> switch (score%10) {
+                case 5,6,7,8,9 -> "D+";
+                default -> "D";
+            };
             default -> "F";
         };
         System.out.printf("%s 점인 학생의 등급은 %s 입니다.", score, grade);
--- src/week05/sec02/homeworks/H06.java
+++ src/week05/sec02/homeworks/H06.java
@@ -10,15 +10,63 @@ public class H06 {
         int month =  input.nextInt();
         System.out.println("월을 입력");
         int day =  input.nextInt();
-
-<<<<<<< HEAD
-        String star = switch (month) {
-            case 1:
+        if(0 < day && day <= 31) {
+            String qufwkfl = switch (month) {
+                case 1 -> {
+                    if (day <= 19) yield "염소자리";
+                    else yield "물병자리";
+                }
+                case 2 -> {
+                    if (day <= 18) yield "물병자리";
+                    else yield "물고기자리";
+                }
+                case 3 -> {
+                    if (day <= 20) yield "물고기자리";
+                    else yield "양자리";
+                }
+                case 4 -> {
+                    if (day <= 19) yield "양자리";
+                    else yield "황소자리";
+                }
+                case 5 -> {
+                    if (day <= 20) yield "황소자리";
+                    else yield "쌍둥이자리";
+                }
+                case 6 -> {
+                    if (day <= 21) yield "쌍둥이자리";
+                    else yield "게자리";
+                }
+                case 7 -> {
+                    if (day <= 22) yield "게자리";
+                    else yield "사자자리";
+                }
+                case 8 -> {
+                    if (day <= 22) yield "사자자리";
+                    else yield "처녀자리";
+                }
+                case 9 -> {
+                    if (day <= 22) yield "처녀자리";
+                    else yield "천칭자리";
+                }
+                case 10 -> {
+                    if (day <= 22) yield "천칭자리";
+                    else yield "전갈자리";
+                }
+                case 11 -> {
+                    if (day <= 22) yield "전갈자리";
+                    else yield "궁수자리";
+                }
+                case 12 -> {
+                    if (day <= 21) yield "궁수자리";
+                    else yield "염소자리";
+                }
+                default -> {
+                    System.out.println("month가 잘못되었습니다..\n입력된 month: " + month);
+                    yield "알 수 없음";
+                }
+            };
+            System.out.printf("%s 월 %s 일 생인 당신의 별자리는 바로!\n%s 랍니다", month, day, qufwkfl);
         }
-=======
-//        String = switch (month) {
-//            case 1:
-//        }
->>>>>>> 5e9b31a6ebdf6626158fd32bd9f3b489b820285e
+        else System.out.println("day가 잘못되었습니다..\n입력된 day: " + day);
     }
 }
