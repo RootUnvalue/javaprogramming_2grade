6029a961e6bfa4e3f42d4a3d7cec0759d604c668
RootUnvalue <mls1218@naver.com>
2026-04-08 22:49:20 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 public static void main(String[] args) {
     System.out.println("Main Page");
-
+    for (int i = 0, j = 10; i <= 5 && j >= 10; i++, j--) {
+        System.out.println("i=" + i + " j=" + j);
+    }
 }
 }
