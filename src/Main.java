d68dcd0a7df5a910b5027bf856aae65893bab6d7
RootUnvalue <mls1218@naver.com>
2026-04-13 23:26:52 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
 public class Main {
+
+    public static class lost{
+        protected int age;
+        private lost(){
+            this.age = 99;
+        }
+    }
 public static void main(String[] args) {
     System.out.println("Main Page");
     for (int i = 0, j = 10; i <= 5 && j >= 10; i++, j--) {
         System.out.println("i=" + i + " j=" + j);
     }
+    String str = "yes";
+    if (str == "yes") System.out.println("true");
+    short a;
+
+    lost as = new lost();
+
+    int x = 10, y = 20;
+    int z = ++x + y++;
+    System.out.println(z + " and " + y);
 }
 }
